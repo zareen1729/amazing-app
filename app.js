a2e6641d7ec608ceb644771ac339598d5ad5b7f6
zareen1729 <zareen1729@gmail.com>
2025-01-09 22:45:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ var server = http.createServer(function (req, res) {
     }
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
+# Listen on port 3000 and IP defaults running on 127.0.0.1
 server.listen(port);
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+# message displays on the terminal
+console.log('Server started running at IP address http://127.0.0.1:' + port + '/');
